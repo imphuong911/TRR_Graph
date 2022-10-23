d80551e9877cc973de53af4e5bd5c651dc49fc29
imphuong911 <buibichphuong911@gmail.com>
2022-10-23 22:38:35 +0700

    menu

---
+++ menu.h
@@ -0,0 +1,25 @@
+void MENU()
+{
+	cout << "     MENU     ";
+	cout << " 1. Nhap du lieu tu file";
+	cout << " 2. In ra man hinh";
+}
+
+void MENUnhap()
+{
+	cout << "    MENU NHAP    ";
+	cout << " 1. Nhap ma tran ke";
+	cout << " 2. Nhap danh sach ke";
+	cout << " 3. Nhap danh sach canh";
+}
+
+
+void MENUin()
+{
+	cout << "    MENU IN    ";
+	cout << " 1. In ma tran ke";
+	cout << " 2. In danh sach ke";
+	cout << " 3. In danh sach canh";
+	cout << " 4. In bac cua dinh";
+}
+
